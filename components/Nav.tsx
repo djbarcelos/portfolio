3a01b616aba7ad1df9087ef5d27cdfc006775b23
djbarcelos <djbarcelos@gmail.com>
2024-08-13 15:30:22 -0300

    feat: inicio site de portfolio

---
+++ components/Nav.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+
+
+const links = [
+    {
+        name: 'inicio',
+        path:'/'
+    },
+    {
+        name: 'projetos',
+        path:'/projetos'
+    },
+    {
+        name: 'contato',
+        path:'/contato'
+    },
+]
+
+export function Nav() {
+    const pathname = usePathname();
+
+    return (
+        <nav className='flex gap-8'>
+            {
+            links.map(link => {
+                return <Link key={link.path} href={link.path} className={`${ link.path === pathname && "text-accent border-b-2 border-accent"
+                } capitalize font-medium hover:text-accent transition-all`}>{link.name}</Link>
+            })
+            }
+        </nav>
+    )
+}
