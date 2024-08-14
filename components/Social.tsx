f059292c9676a04c340883f3564f1374effa9b8b
djbarcelos <djbarcelos@gmail.com>
2024-08-14 11:52:39 -0300

    feat: finalizado homepage

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,5 +1,35 @@
 import React from 'react'
+import Link from 'next/link'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { BiLogoGmail } from 'react-icons/bi'
 
-export function Social() {
-  return <div>social</div>
+type SocialItem = {
+  icon: JSX.Element
+  path: string
+}
+
+type SocialProps = {
+  containerStyles?: string
+  iconStyles?: string
+}
+
+const socials: SocialItem[] = [
+  { icon: <FaGithub />, path: '' },
+  { icon: <FaLinkedinIn />, path: '' },
+  { icon: <BiLogoGmail />, path: '' },
+]
+
+export function Social(props: SocialProps) {
+  const { containerStyles, iconStyles } = props
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        )
+      })}
+    </div>
+  )
 }
